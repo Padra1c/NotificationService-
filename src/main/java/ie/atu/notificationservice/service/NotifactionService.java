dd18604c9e5796423aa2164002881eab064a0218
PadraicOLoughlin-STU <g00468865@atu.ie>
2026-03-25 10:59:55 +0000

    changes

--- src/main/java/ie/atu/notificationservice/service/NotifactionService.java
+++ src/main/java/ie/atu/notificationservice/service/NotifactionService.java
@@ -1,29 +1,6 @@
 package ie.atu.notificationservice.service;
 
 import ie.atu.notificationservice.clients.ReservationClient;
-import ie.atu.notificationservice.model.Reservation;
 import org.springframework.stereotype.Service;
 
-import javax.management.Notification;
-import java.time.LocalDate;
-
 @Service
-public class NotifactionService {
-    private final ReservationClient reservationClient;
-
-    public NotifactionService(ReservationClient reservationClient) {
-        this.reservationClient = reservationClient;
-    }
-
-
-    public Notification createNotification(Long reservationID)
-    {
-        Reservation reservation = reservationClient.getReservation(reservationID);
-        Notification notification = new Notification();
-        notification.setReservationId(reservationID);
-        notification.setEquipmentTag(reservation.getEquipmentTag());
-        notification.setStudentEmail(reservation.getStudentEmail());
-        notification.setNotificationDate(LocalDate.now());
-        return notification;
-    }
-}
